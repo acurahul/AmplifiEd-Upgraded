d8656f14e5df53a174eb6ce13fcb0a30ac9405b7
acurahul <acutantrahul@gmail.com>
2025-09-20 18:50:21 +0530

    Disable MSW to Fix All Errors

--- src/main.tsx
+++ src/main.tsx
@@ -5,17 +5,8 @@ import './index.css';
 
 async function enableMocking() {
   if (import.meta.env.DEV) {
-    const { worker } = await import('../mocks/browser.ts');
-    return worker.start({
-      onUnhandledRequest: 'bypass',
-    });
-  }
-}
-
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-});
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
